abdc42eb01282364a531562698c212205a3e0ceb
Pranav Bala <technotebook@yahoo.com>
2022-01-13 13:50:52 -0600

    computer client complete - sends msg based on arrow keys

--- computer.py
+++ computer.py
@@ -1,4 +1,5 @@
 import socket
+import curses
 
 HEADER = 64
 PORT = 5050  # Port for server to run on
@@ -20,4 +21,39 @@ def send(msg):
     client.send(message)
 
 
+# get the curses screen window
+screen = curses.initscr()
 
+# turn off input echoing
+curses.noecho()
+
+# respond to keys immediately (don't wait for enter)
+curses.cbreak()
+
+# map arrow keys to special values
+screen.keypad(True)
+
+try:
+    while True:
+        char = screen.getch()
+        if char == ord('q'):
+            break
+        elif char == curses.KEY_RIGHT:
+            # print doesn't work with curses, use addstr instead
+            screen.addstr(0, 0, 'right')
+            send('right')
+        elif char == curses.KEY_LEFT:
+            screen.addstr(0, 0, 'left ')
+            send('left')
+        elif char == curses.KEY_UP:
+            screen.addstr(0, 0, 'up   ')
+            send('forward')
+        elif char == curses.KEY_DOWN:
+            screen.addstr(0, 0, 'down ')
+            send('reverse')
+finally:
+    # shut down cleanly
+    curses.nocbreak()
+    screen.keypad(0)
+    curses.echo()
+    curses.endwin()
