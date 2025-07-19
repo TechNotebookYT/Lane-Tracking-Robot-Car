fa6e1338710b01a1f87175c8726b833d7dab03ef
Pranav Bala <technotebook@yahoo.com>
2025-07-19 13:09:47 -0500

    tweaked parameters for v5

--- v5/main.py
+++ v5/main.py
@@ -3,6 +3,7 @@ import cv2
 from LaneDetection import getLaneCurve
 import WebcamMod
 from ImageStreamer import ImageStreamer
+import time
  
 # Initialize MotorController
 car = MotorController((2, 3, 4), (22, 27, 17), leftBias=1, rightBias=1)
@@ -13,31 +14,36 @@ def main():
     curveVal, frame = getLaneCurve(img)
     streamer.update_image(frame)
     
-    threshold_to_turn = 20
+    threshold_to_turn = 30
     
     # Check to turn right
     if curveVal > threshold_to_turn:
-        car.turnRight(speed=100)
+        car.turnRight(speed=35)
+        # time.sleep(0.03)
         print("turnright")
     
     # Check to turn left
     elif curveVal < -threshold_to_turn:
-        car.turnLeft(speed=100)
+        car.turnLeft(speed=35)
+        # time.sleep(0.03)
         print("turnleft")
 
     else:
-        car.moveForward(speed=100)
+        car.moveForward(speed=30)
 
 try: 
     if __name__ == '__main__':
         streamer = ImageStreamer(port=8000)
         streamer.start()
-
+        print("5 seconds til robot starts")
+        time.sleep(5)
         
         while True:
             main()
+            # time.sleep(0.05)
             # cv2.waitKey(7) # Needed to display elements on screen
 finally:
     car.stop() # Stops the Car
-    car.exit() # Runs Pin Cleanup script
+    time.sleep(0.2)
+    # car.exit() # Runs Pin Cleanup script
     streamer.stop() # Stops the Image Streamer
