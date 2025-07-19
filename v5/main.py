cd9a0adea85cbb439329db085c4dd47ef33fe2e4
Pranav Bala <technotebook@yahoo.com>
2025-07-18 20:54:04 -0500

    modified main runner file to incorporate new image streamer

--- v5/main.py
+++ v5/main.py
@@ -2,40 +2,42 @@ from MotorController import MotorController
 import cv2
 from LaneDetection import getLaneCurve
 import WebcamMod
+from ImageStreamer import ImageStreamer
  
 # Initialize MotorController
 car = MotorController((2, 3, 4), (22, 27, 17), leftBias=1, rightBias=1)
 
  
 def main():
- 
     img = WebcamMod.getImg()
-    curveVal = getLaneCurve(img)
+    curveVal, frame = getLaneCurve(img)
+    streamer.update_image(frame)
     
-    threshold_to_turn = 15
+    threshold_to_turn = 20
     
     # Check to turn right
     if curveVal > threshold_to_turn:
-        car.turnRight(speed=30)
+        car.turnRight(speed=100)
         print("turnright")
     
     # Check to turn left
     elif curveVal < -threshold_to_turn:
-        car.turnLeft(speed=30)
+        car.turnLeft(speed=100)
         print("turnleft")
 
     else:
-        car.moveForward(speed=25)
-        
+        car.moveForward(speed=100)
 
-    
-
-     
 try: 
     if __name__ == '__main__':
+        streamer = ImageStreamer(port=8000)
+        streamer.start()
+
+        
         while True:
             main()
-            cv2.waitKey(7) # Needed to display elements on screen
+            # cv2.waitKey(7) # Needed to display elements on screen
 finally:
     car.stop() # Stops the Car
     car.exit() # Runs Pin Cleanup script
+    streamer.stop() # Stops the Image Streamer
